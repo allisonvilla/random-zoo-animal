1587b5c9a17a088e58e1f286700d3a2fc0720e18
allison <allison.villa@outlook.com>
2022-02-13 12:47:33 -0500

    added pseudocode and html

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const zooApp = {}
+
+zooApp.animalName = documnent.querySelector('.animal-name'); 
+zooApp.animalImg = documnent.querySelector('.img-container'); 
+zooApp.animalInfo = document.querySelector('.animal-info'); 
+
 fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
     .then(function(response) {
         return response.json();
     })
     .then(function(jsonResponse) {
         console.log(jsonResponse);
+        // Put stuff from the jsonResponse into variables
     });
+
+// Have an input (drop-down menu) that lets the user choose what type of animal they would like to see
