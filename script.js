58da7df6fb28ea6e5ea503372e088650067b5e04
allison <allison.villa@outlook.com>
2022-02-14 12:01:34 -0500

    organized js better

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 const zooApp = {}
 
+zooApp.init = function() {
+    zooApp.getData();
+}
+
 zooApp.nameEl = document.querySelector('.animal-name'); 
 zooApp.imgEl = document.querySelector('.img-container'); 
 zooApp.infoEl = document.querySelector('.animal-info'); 
 
-fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(jsonResponse) {
-        console.log(jsonResponse);
-        // Put stuff from the jsonResponse into variables and add to DOM
-        zooApp.nameEl.textContent = jsonResponse.name;
-        zooApp.animalImg = document.createElement('img');
-        zooApp.animalImg.src = jsonResponse.image_link; 
-        zooApp.imgEl.appendChild(zooApp.animalImg); 
-        // Can use for-in loop to print items inside the object into the infoEl?
-        for (const property in jsonResponse) {
-            zooApp.listEl = document.createElement('li');
-            zooApp.listEl.textContent = `${property}: ${jsonResponse[property]}`;
-            zooApp.infoEl.appendChild(zooApp.listEl);  
-        }
-        // I don't want it to display absolutely everything, so I should specify which properties I want shown on the page
-    });
+zooApp.getData = function() {
+    fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsonResponse) {
+            zooApp.displayData(jsonResponse);
+        });
+    }
+    
+zooApp.displayData = function(apiData) {
+    // Put stuff from the jsonResponse into variables and add to DOM
+    zooApp.nameEl.textContent = apiData.name;
+    zooApp.animalImg = document.createElement('img');
+    zooApp.animalImg.src = apiData.image_link; 
+    zooApp.imgEl.appendChild(zooApp.animalImg); 
+
+    // Can use for-in loop to print items inside the object into the infoEl?
+    for (const property in apiData) {
+        zooApp.listEl = document.createElement('li');
+        zooApp.listEl.textContent = `${property}: ${apiData[property]}`;
+        zooApp.infoEl.appendChild(zooApp.listEl);  
+    }
+
+    // I don't want it to display absolutely everything, so I should specify which properties I want shown on the page
+    
+}
 
 // Have an input (drop-down menu) that lets the user choose what type of animal they would like to see
+
+zooApp.init();
