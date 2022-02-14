9aa3e20c659a0c747445530d39bf9e17ac870328
allison <allison.villa@outlook.com>
2022-02-14 11:09:06 -0500

    saving progress on displaying info

--- script.js
+++ script.js
@@ -14,12 +14,14 @@ fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
         zooApp.nameEl.textContent = jsonResponse.name;
         zooApp.animalImg = document.createElement('img');
         zooApp.animalImg.src = jsonResponse.image_link; 
-        zooApp.imgEl.appendChild = zooApp.animalImg; 
+        zooApp.imgEl.appendChild(zooApp.animalImg); 
         // Can use for-in loop to print items inside the object into the infoEl?
         for (const property in jsonResponse) {
-
+            zooApp.listEl = document.createElement('li');
+            zooApp.listEl.textContent = `${property}: ${jsonResponse[property]}`;
+            zooApp.infoEl.appendChild(zooApp.listEl);  
         }
-        console.log(zooApp.animalName);
+        // I don't want it to display absolutely everything, so I should specify which properties I want shown on the page
     });
 
 // Have an input (drop-down menu) that lets the user choose what type of animal they would like to see
