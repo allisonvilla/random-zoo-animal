36c17a74194c64c4ee08f0ed70525fd512ff9cd1
allison <allison.villa@outlook.com>
2022-02-14 10:31:41 -0500

    why isnt my image showing up

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const zooApp = {}
 
-zooApp.animalName = documnent.querySelector('.animal-name'); 
-zooApp.animalImg = documnent.querySelector('.img-container'); 
-zooApp.animalInfo = document.querySelector('.animal-info'); 
+zooApp.nameEl = document.querySelector('.animal-name'); 
+zooApp.imgEl = document.querySelector('.img-container'); 
+zooApp.infoEl = document.querySelector('.animal-info'); 
 
 fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
     .then(function(response) {
@@ -10,7 +10,16 @@ fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
     })
     .then(function(jsonResponse) {
         console.log(jsonResponse);
-        // Put stuff from the jsonResponse into variables
+        // Put stuff from the jsonResponse into variables and add to DOM
+        zooApp.nameEl.textContent = jsonResponse.name;
+        zooApp.animalImg = document.createElement('img');
+        zooApp.animalImg.src = jsonResponse.image_link; 
+        zooApp.imgEl.appendChild = zooApp.animalImg; 
+        // Can use for-in loop to print items inside the object into the infoEl?
+        for (const property in jsonResponse) {
+
+        }
+        console.log(zooApp.animalName);
     });
 
 // Have an input (drop-down menu) that lets the user choose what type of animal they would like to see
